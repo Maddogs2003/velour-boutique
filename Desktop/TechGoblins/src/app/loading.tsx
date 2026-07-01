b0c99493963217a28fb6d050f3ab61d5bb99a62f
Maddogs2003 <nitrogabi@gmail.com>
2026-07-01 16:25:32 +0200

    chore: remove orphaned files from wrong git root

--- Desktop/TechGoblins/src/app/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="flex items-center justify-center h-96">
-      <div className="w-6 h-6 border-2 border-stone-200 border-t-stone-700 rounded-full animate-spin" />
-    </div>
-  );
-}
