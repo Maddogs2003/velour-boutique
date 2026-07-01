8b72e3670d421a61c7a9972b937a59e4003e4927
Maddogs2003 <nitrogabi@gmail.com>
2026-07-01 16:43:40 +0200

    feat: new brand palette (purple/crimson/orange/cream) + remove categories & editorial sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
 import { Hero } from "@/components/home/hero";
-import { Categories } from "@/components/home/categories";
 import { FeaturedProducts } from "@/components/home/featured-products";
 import { BrandValues } from "@/components/home/brand-values";
-import { EditorialBanner } from "@/components/home/editorial-banner";
 
 export default function HomePage() {
   return (
     <>
       <Hero />
       <BrandValues />
-      <Categories />
       <FeaturedProducts />
-      <EditorialBanner />
     </>
   );
 }
